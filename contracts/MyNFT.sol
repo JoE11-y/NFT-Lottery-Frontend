7e3ef6a034fdc7a28b3e6ba691faed070ac3be63
= <Emmanuelsumeh@gmail.com>
2022-02-07 17:08:43 +0100

    feat: boilerplate smart contract and ui components

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
 // ################################# NFT SMART CONTRACT GOES HERE ######################################
+pragma solidity ^0.8.2;
+
+contract MyNFT {
+    uint public count;
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        count -= 1;
+    }
+}
