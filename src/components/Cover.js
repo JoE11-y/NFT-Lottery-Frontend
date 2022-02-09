b090632a0fe5bd916108bed5752e1066a5b7c479
moritzfelipe <stellmacher@gmail.com>
2022-02-09 14:20:34 +0100

    enhancement: move connect out of cover component

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,34 +1,26 @@
 import logo from "../logo.svg";
-import {Button} from "react-bootstrap";
-import {useContractKit} from "@celo-tools/use-contractkit";
+import { Button } from "react-bootstrap";
 
-const Cover = () => {
-    const {connect} = useContractKit()
-
-    const connectWallet = async () => {
-
-        try {
-            await connect()
-        } catch (e) {
-            console.log({e})
-        }
-
+const Cover = ({ connect }) => {
+  const connectWallet = async () => {
+    try {
+      await connect();
+    } catch (e) {
+      console.log({ e });
     }
-    return (
-        <>
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Celo React Boilerplate
-            </p>
+  };
+  return (
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Celo React Boilerplate</p>
 
-            <p>
-                Lets get started!
-            </p>
+      <p>Lets get started!</p>
 
-            <Button variant="primary" onClick={connectWallet}>Connect Wallet</Button>
-        </>
+      <Button variant="primary" onClick={connectWallet}>
+        Connect Wallet
+      </Button>
+    </>
+  );
+};
 
-    )
-}
-
-export default Cover
+export default Cover;
