d92b8d4ada2920cd02520ae0e6556b91fa1fe9ab
JoE11-y <blockydevjoe@gmail.com>
2022-07-20 04:41:24 +0100

    added transactionhash to console output

--- scripts/getWinningTickets.js
+++ scripts/getWinningTickets.js
@@ -2,16 +2,24 @@ const hre = require("hardhat");
 const utils = require("./utils");
 
 async function main() {
-    const lotteryAddress = utils.getLotteryAddress();
-    const NFTLotteryFactory = await hre.ethers.getContractFactory("NFTLottery");
-    const NFTLottery = NFTLotteryFactory.attach(lotteryAddress);
-    const txn = await NFTLottery.getWinningTickets();
-    await txn.wait();
+  const lotteryAddress = utils.getLotteryAddress();
+  const NFTLotteryFactory = await hre.ethers.getContractFactory("NFTLottery");
+  const NFTLottery = NFTLotteryFactory.attach(lotteryAddress);
+  const txn = await NFTLottery.getWinningTickets();
+  await txn.wait();
+
+  const transactionHash = `https://alfajores-blockscout.celo-testnet.org/tx/${txn.hash}/internal-transactions`;
+
+  console.log("Winning Tickets Gotten");
+
+  console.log("Transaction Hash: ", { transactionHash });
 }
 
-main().then(() => {
+main()
+  .then(() => {
     process.exit(0);
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
     process.exit(1);
-})
+  });
